4fa45c7cedd3c7d2adfd451138d70a01d8a90e93
nsyout <g@nsyout.com>
2026-02-12 16:43:20 -0600

    nvim colorscheme and which-key

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -5,5 +5,15 @@ return {
     opts = {
       delay = 400,
     },
+    config = function(_, opts)
+      local wk = require("which-key")
+      wk.setup(opts)
+      wk.add({
+        { "<leader>g", group = "Git" },
+        { "<leader>h", group = "Harpoon" },
+        { "<leader>n", group = "Search" },
+        { "<leader>s", group = "Search" },
+      })
+    end,
   },
 }
